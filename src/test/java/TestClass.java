d35e7bad3fcb49161584c0f13ab655790af72dbf
jidvorak <jidvorak@seznam.cz>
2023-12-14 23:13:36 +0100

    Init java advanced

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -3,7 +3,8 @@ import org.junit.jupiter.api.Test;
 
 
 public class TestClass {
-
+    // TOKEN
+    // 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
     @Test
     public void exceptionsTest(){
         ExcClass ec = new ExcClass();

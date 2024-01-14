c5011885d2ba7e8d5e66a286f674c8b9894ff1dd
jidvorak <jidvorak@seznam.cz>
2024-01-14 13:25:20 +0100

    Init java advanced

--- src/test/java/GenericsTest.java
+++ src/test/java/GenericsTest.java
@@ -1,14 +1,15 @@
 import colections.DataClass;
-import generisc.Box;
-import generisc.GenBox;
-import generisc.GenCup;
+import generisc.*;
 import interfacebest.Child;
 import interfacebest.ManInteface;
 import interfacebest.OldMan;
 import org.junit.jupiter.api.Test;
 
+import java.lang.management.ManagementPermission;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GenericsTest {
 
@@ -115,6 +116,7 @@ public class GenericsTest {
     @Test
     public void testConstructor() {
 
+        // genericka trida ktere v konstruktoru predavame data
         ManInteface man = new OldMan();
         GenCup<ManInteface> cup = new GenCup<>(man);
 
@@ -123,6 +125,75 @@ public class GenericsTest {
         GenCup<Integer> cup03 = new GenCup<>(11);
 
     }
+
+    @Test
+    public void pairTest(){
+
+        // vytvoreni genericke tridy klic/hodnota s typy Integer/DataClass
+        Pair<Integer, DataClass> pair = new Pair<>();
+        pair.setKey(1); // nastaveni klice
+        pair.setValue(new DataClass("first", 11)); // nastaveni hodnoty
+        Integer key01 = pair.getKey(); // precteni klice
+        DataClass dataClass01 = pair.getValue(); // precteni hodnoty
+        System.out.println("klic=" + key01 + " - hodnota=" + dataClass01);
+
+        // stejne je konstruovana mapa (do mapy se dá přidávat více záznamů - je to kolekce)
+        Map<Integer, DataClass> map = new HashMap<>();
+
+    }
+
+    @Test
+    public void pairChils(){
+
+        // Potomek muze pracovat klicem definovanym zde jako <String>
+        // hodnota byla definovana jako Integer pri dedeni v tride PairChild01
+        PairChild01<String> child01 = new PairChild01<>();
+        child01.setKey("some string11");
+        child01.setValue(555);
+
+        // potomek muze pracovat jen s <Integer, String>
+        // to bylo deklarovano v tride PairChild02 ( extends Pair<Integer, String> )
+        PairChild02 pchild = new PairChild02();
+        pchild.setKey(11);
+        pchild.setValue("stingX");
+
+    }
+
+    @Test
+    public void genMethod(){
+
+        // generická metoda převezme jen takové typy parametrů, jaké jsou deklarovány
+        // v promenné ( Pair<Integer, String> firstPair ) tedy Integer + String
+        Pair<Integer, String> firstPair = GeneriMethod.generatePair(1, "abcd");
+        Pair<Integer, DataClass> nextPair = GeneriMethod.generatePair(1, new DataClass("name", 11));
+
+        // TODO - dodelat
+        /*
+        String str = GeneriMethod.getString("teststr");
+        String s2 = GeneriMethod.getString(111);
+         */
+    }
+
+    @Test
+    public void genLimits(){
+
+        // genericka trida NumericBox definuje generiku jako <T extends Number>
+        // proto jdou pouzit jen typy zdedene z Number
+
+        // Integer jde pouzit, dedi z Number
+        NumericBox<Integer> numBox = new NumericBox<>();
+        numBox.setItem(11);
+        // Double jde pouzit, dedi z Number
+        NumericBox<Double> numDabBox = new NumericBox<>();
+        numDabBox.setItem(11.1);
+
+        /*
+        // nejde pouzit String - nededi z Number
+        NumericBox<String> numStrBox = new NumericBox<>();
+        numStrBox.setItem("");*/
+
+    }
+
 
 }
 

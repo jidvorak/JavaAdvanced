83a63c95d18b1590e4eac810af6b572c238f0807
jidvorak <jidvorak@seznam.cz>
2024-01-20 11:01:53 +0100

    Init java advanced

--- src/test/java/InputOutputTests.java
+++ src/test/java/InputOutputTests.java
@@ -1,10 +1,25 @@
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class InputOutputTests {
 
     protected static final String C_ROOTDIR = "C:/JavaIO";
+
+    // **************** BASIC FILE *******************************
+
+    @Test
+    public void sysProperty(){
+        // pouzity system property
+        // vraci data dle opreracniho systemu WIN, MAC, LINUX
+        System.out.println(System.getProperty("file.separator")); // oddelovac cesty k soubopru
+        System.out.println(System.getProperty("line.separator")); // oddelovac radku textu
+
+        // deklarace cesty s promenou prostredi dle operacniho systemu
+        //protected static final String C_ROOTDIR = "C:" + System.getProperty("file.separator") + "JavaIO";
+    }
 
     @Test
     public void basicIOTest() throws Exception {
@@ -16,9 +31,8 @@ public class InputOutputTests {
         } else {
             System.out.println("file NOT exists");
         }
-        file.createNewFile();
+        file.createNewFile(); // prejmenovani
 
-        //file.renameTo(new File(C_ROOTDIR + "/textfile_new_name.txt"));
     }
 
     @Test
@@ -49,4 +63,29 @@ public class InputOutputTests {
         }
 
     }
+
+     // **************** STREAM *******************************
+
+    @Test
+    public void streamTest() throws Exception {
+        FileInputStream in = null; // vstupni souborovy stream
+        FileOutputStream out = null; // vystupni souborovy stream
+        try{
+            in = new FileInputStream(C_ROOTDIR + "/source.jpg"); // vytvoreni in strem s cestou ke zdrojovemu souboru
+            out = new FileOutputStream(C_ROOTDIR + "/new.jpg"); // vytvoreni out strem s cetou k cilovamu souboru
+            int c;
+            while((c = in.read()) != -1){ // nacteme
+                out.write(c); // vypiseme
+            }
+        }finally {
+            if(in !=null){
+                in.close();
+            }
+            if(out !=null){
+                out.close();
+            }
+        }
+    }
+
+
 }
